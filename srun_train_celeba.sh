48b0ad97d745a4587a40acc6ab8e5b24b889d021
Rafał Michaluk <rafal.michaluk@codility.com>
2021-10-22 13:05:47 +0200

    remove name from sruns

--- srun_train_celeba.sh
+++ srun_train_celeba.sh
@@ -1,4 +1,4 @@
-source /home/ptempczyk/virtualenvs/glow-pytorch/bin/activate
+source ~/virtualenvs/glow-pytorch/bin/activate
 for d in `cat ./parameters/celeba.deltas`
 do
     srun --partition=common --qos=8gpu7d --gres=gpu:1 python train.py --delta="$d" `cat ./parameters/celeba.params` --device cuda:0 &
