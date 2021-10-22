a9f9454d49f92981ade0ba3022689d70bbfd77a5
Rafał Michaluk <rm360179@students.mimuw.edu.pl>
2021-10-22 05:32:49 +0200

    remove some user paths

--- srun_train_cifar_horses_40.sh
+++ srun_train_cifar_horses_40.sh
@@ -1,4 +1,4 @@
-source /home/rm360179/glow-pytorch/venv/bin/activate
+source ~/glow-pytorch/venv/bin/activate
 for d in `cat ./parameters/cifar_horses_40.deltas`
 do
     srun --partition=common  --qos=16gpu3d --gres=gpu:1 --time 3-0 python train.py --delta="$d" `cat ./parameters/cifar_horses_40.params`  --device cuda:0   &
--- srun_train_mnist.sh
+++ srun_train_mnist.sh
@@ -1,4 +1,4 @@
-source /home/ptempczyk/virtualenvs/glow-pytorch/bin/activate
+source ~/glow-pytorch/venv/bin/activate
 for d in `cat ./parameters/mnist.deltas`
 do
     srun --partition=common --qos=16gpu3d --gres=gpu:1 python train.py --delta="$d" `cat ./parameters/mnist.params` --device cuda:0 &
